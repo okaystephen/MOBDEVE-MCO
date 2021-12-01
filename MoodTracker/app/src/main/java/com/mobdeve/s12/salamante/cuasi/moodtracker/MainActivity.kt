edcee53b51aa97c5836607f364fe3124d17c21c8
Beaverly Cuasi <61379865+recklessbeav@users.noreply.github.com>
2021-12-01 16:03:22 +0800

    binding not working

--- MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/moodtracker/MainActivity.kt
+++ MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/moodtracker/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.mobdeve.s12.salamante.cuasi.moodtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdeve.s12.salamante.cuasi.moodtracker.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    var binding:ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+
     }
 }
